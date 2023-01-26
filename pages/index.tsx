6e88bcec6c5d6c3f966000e09ec1277bd0a6e3fe
Tsz San Sam, CHAN <samchan148@gmail.com>
2023-01-26 12:42:25 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,6 @@
-import clsx from "clsx";
-import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Image from "next/image";
-import Link from "next/link";
 
 import HeadFactory from "lib/HeadFactory";
-import landingBannerMobile from "public/landing-banner-mobile.png";
-import landingBannerPng from "public/landing-banner.png";
-import dareToExploreBannerPng from "public/landing-dare-to-explore.png";
-import landingEarthMobilePng from "public/landing-earth-mobile.png";
-import landingEarthPng from "public/landing-earth.png";
 
 import type { GetStaticPropsContext } from "next";
 import type { PropsWithoutRef } from "react";
@@ -17,10 +8,6 @@ import type { PropsWithoutRef } from "react";
 type Props = {
   locale: string;
 };
-
-const bannerStyle: TwClassName = "bg-yellow-100 relative w-full overflow-hidden";
-const ctaButtonStyle: TwClassName =
-  "flex items-center justify-center px-[30px] rounded-[62px] border-2 border-solid border-neutral-900 bg-yellow-500 tablet:px-[56px] py-4 text-label tablet:text-h5 font-bold text-neutral-900";
 
 export default function Home({ locale }: PropsWithoutRef<Props>) {
   return (
